ac4edcb8a1af37d23a4ae4b5094d9e1a424090d3
Gleb Kalinin <g.kalinin@iqdev.digital>
2021-08-29 19:42:07 +0500

    codewars: kyu7

--- main.go
+++ main.go
@@ -1,60 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	//x := 10.0
-	//n := 5.0
-	//lim := 2.5
-	//pow(x, n, lim)
-
-	//input := int64(10)
-	//getSwitch(input)
-
-	checkValue := int32(0)
-	getIfSwitch(checkValue)
+	var n = 10
+	Divisors(n)
 }
 
-func getIfSwitch(checkValue int32) {
-	fmt.Println(checkValue)
-	switch {
-	case checkValue <= 0:
-		checkValue = 10
-		getIfSwitch(checkValue)
-	case checkValue <= 10:
-		checkValue--
-		if checkValue != 0 {
-			getIfSwitch(checkValue)
+func Divisors(n int) int {
+	count := 0
+
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			count += 1
 		}
 	}
-}
-
-// math.pow = Возведение в число
-func pow(x, n, lim float64) float64 {
-	if result := math.Pow(x, n); result < lim {
-		fmt.Println(result)
-	}
-	fmt.Println(lim)
-	return lim
-}
-func getSwitch(input int64) int64 {
-	//var checkValue uint32
-	//fmt.Scan(&checkValue)
-	switch input {
-	case 0:
-		fmt.Println("Zero")
-		return 1
-	case 1:
-		fmt.Println("One")
-		return 1
-	case 2:
-		fmt.Println("Two")
-		return 2
-	default:
-		fmt.Println("Default")
-		return 0
-	}
+	fmt.Println(count)
+	return count
 }
